7ea8a0f5af1780aa018154e295cac03cd6c32420
wzlw <zl2011sk@163.com>
2020-10-04 21:02:19 +0800

    week6 server.js

---
+++ Week_06/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+  let body = [];
+  request.on('error', (error) => {
+    console.log(error);
+  }).on('data', (chunk) => {
+    body.push(chunk.toString());
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    console.log(body);
+    response.writeHead(200, {'Content-type': 'text/html'})
+    response.end('hello world \n');
+  })
+}).listen(8081);
+
+console.log('sever is started');
